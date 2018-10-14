4c6d5fc92f75e04989f95964d27db4bf32fc5798
Clayton Stamper <clayton@Claytons-MacBook-Pro.local>
2018-10-14 18:16:56 -0500

    a2c is done but has random Ms in the output

--- a2c.asm
+++ a2c.asm
@@ -3,12 +3,16 @@
 			.data
 intArr:			.word 111, 222, 333, 444
 seperator:		.asciiz ", "
-arrOutput:		.asciiz "Here are is the array contents: "
+arrOutput:		.asciiz "Here are the array contents: "
+arrOutputRev:		.asciiz "Here are the array contents swapped & reversed: "
+swapMsg:		.asciiz "swapping... \n"
 
 			.text
 			.globl main
 			
 main:
+
+#print arr
 
 			li $v0, 4
 			la $a0, arrOutput
@@ -46,9 +50,75 @@ main:
 			lw $t0, 12($a0)
 			move $a0, $t0
 			syscall # arr[3]
+			li $v0, 11
+			li $a0, '\n'
+			syscall
+			
+			
+#swap arr
+			
+			li $v0, 4
+			la $a0, swapMsg
+			syscall
+			
+			la $a0, intArr
+			
+			lw $t0, 0($a0)
+			lw $t1, 8($a0)
+			
+			sw $t0, 8($a0)
+			sw $t1, 0($a0)
+			
+			syscall
+			
+			la $a0, intArr
+			
+			lw $t0, 4($a0)
+			lw $t1, 12($a0)
+			
+			sw $t0, 12($a0)
+			sw $t1, 4($a0)
+			
+			syscall
+			
+#print arr swapped and reversed
+			
+			li $v0, 4
+			la $a0, arrOutputRev
+			syscall #initial label
+			
+			li $v0, 1
+			la $a0, intArr
+			lw $t0, 12($a0)
+			move $a0, $t0
+			syscall # arr[0]
 			li $v0, 4
 			la $a0, seperator
 			syscall
+			
+			li $v0, 1
+			la $a0, intArr
+			lw $t0, 8($a0)
+			move $a0, $t0
+			syscall # arr[1]
+			li $v0, 4
+			la $a0, seperator
+			syscall
+			
+			li $v0, 1
+			la $a0, intArr
+			lw $t0, 4($a0)
+			move $a0, $t0
+			syscall # arr[2]
+			li $v0, 4
+			la $a0, seperator
+			syscall
+			
+			li $v0, 1
+			la $a0, intArr
+			lw $t0, 0($a0)
+			move $a0, $t0
+			syscall # arr[3]
 			
 			
 			li $v0, 10
